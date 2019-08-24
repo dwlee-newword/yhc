3dc3020ea36a4ae1cff7a21f35dfc2a4ac267616
dwlee-newword <dwlee.newword@gmail.com>
2019-08-24 18:55:34 +0900

    vector simple operation

---
+++ vector/src/1/simple_operation.py
@@ -0,0 +1,41 @@
+import numpy as np
+# 수학적 연산을 하는 라이브러리
+
+# 2D list
+listA = [1, 3, 5, 7]
+listB = [2, 4, 6, 8]
+
+# 2D vector
+vectorA = np.array([1, 3, 5, 7])
+vectorB = np.array([2, 4, 6, 8])
+
+# list와 vector의 연산 차이 
+# 두 리스트의 합은?
+
+print(listA + listB)
+
+# 두 벡터의 합은?
+
+print(vectorA + vectorB)
+
+# list와 vector는 합에서 차이가 나는 것을 볼 수 있음
+
+# list와 vector의 곱 연산
+# 리스트의 단순(scala) 곱은?
+
+print(listA * 2)
+
+# 두 리스트의 곱은?
+
+# print(listA * listB)
+#print(listA * [2])
+
+# 벡터의 단순(scala) 곱은?
+
+print(vectorA * 2)
+
+# 두 벡터의 곱은?
+
+print(vectorA * vectorB)
+
+
